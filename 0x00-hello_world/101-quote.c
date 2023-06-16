2e8fc7d51c463d54bd1ca1d8ea90e9a89ff3cf79
Naomi <naomielvis26@gmail.com>
2023-06-15 19:26:50 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(message) - 1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
