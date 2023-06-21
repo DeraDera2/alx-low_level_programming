729052db6bf8f35704d86e50b7291f9b44fdc4da
Naomi <naomielvis26@gmail.com>
2023-06-21 15:21:28 -0700

    push

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Description: Prints to 98 and back
+ * Return - void
+ * @n: parameter
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n != 98)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
