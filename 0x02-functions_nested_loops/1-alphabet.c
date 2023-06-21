a67d48cd8dcde3b9da7192a1e8ce13a21a229caf
Naomi <naomielvis26@gmail.com>
2023-06-21 10:20:18 -0700

    Omogit add .!

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+/*Function prototype*/
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ *
+ * Return - Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+}
+
