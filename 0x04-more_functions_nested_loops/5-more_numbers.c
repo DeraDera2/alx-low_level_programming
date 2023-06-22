b17f8340d486e3e8ddf0878a66cd630b3117ca83
Naomi <naomielvis26@gmail.com>
2023-06-22 12:44:13 -0700

    ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,9 @@ void more_numbers(void)
 	{
 		for (int j = 0; j <= 14; j++)
 		{
-			if (j > 9)
-			{
-				_putchar((j / 10) + '0');
-			}
-			_putchar((j % 10) + '0');
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
