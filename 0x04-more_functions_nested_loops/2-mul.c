147ff4abea94e0424e436fd8baf2a0d99db72a19
Naomi <naomielvis26@gmail.com>
2023-06-22 10:08:00 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Description: Multiplies two integers
+ * Return: Int
+ * @a: parameter
+ * @b: parameter
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 int _isupper(int c);
 void _putchar(char c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
