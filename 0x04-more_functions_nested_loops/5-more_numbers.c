8e035da80582cd5767f94540b87739f1d3e1dc89
Naomi <naomielvis26@gmail.com>
2023-06-22 12:32:02 -0700

    ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - Description: Prints 1-14 ten times
+ * Return - Always void
+ */
+void more_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
