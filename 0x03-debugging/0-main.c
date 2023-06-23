3072ca9291a059dab0d794cb56665a18bf30871a
Naomi <naomielvis26@gmail.com>
2023-06-22 18:08:22 -0700

    debu~g

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
-* @i: parameter
 */
-void positive_or_negative(int i)
-{
-	int i;
 
-	i = 98;
+int main(void)
+{
+	int i = 98;
+	
 	positive_or_negative(i);
 	return (0);
 }
