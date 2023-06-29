768b31382368c572cc371e587e833b0dbe0de49b
Naomi <naomielvis26@gmail.com>
2023-06-29 13:12:24 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - Description: Copies a string
+ * Return: A string
+ * @dest: Destination atring
+ * @src: Source string
+ * @n: Number of characters to copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
