ff6242269ebea4d1ef4b0115acaa9bde5ee2e7f7
Naomi <naomielvis26@gmail.com>
2023-06-30 11:46:55 -0700

    Route 13 encryption

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - Description: Encrypts in rot13
+ * Return: A pointer to the modified string
+ * @s: String to be modified
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
