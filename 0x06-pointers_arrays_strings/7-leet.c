31962db1bcd689349989e738e6dad3eb206ecf08
Naomi <naomielvis26@gmail.com>
2023-06-30 09:56:19 -0700

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - Description: Encodes a string
+ * @n: String
+ * Return: String
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; j++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
